ae8dde1579fc3b5e44629c8a8ed42b44fb436eea
Sakib Fakir <FakirSakib22232@gmail.com>
2025-09-11 16:56:58 +0600

    npm pkg removed

--- plan.js
+++ plan.js
@@ -5,8 +5,9 @@ const fromValue = () => {
     const title = document.getElementById("title").value
     const textArea = document.getElementById("text-area").value;
     const datePicker = document.getElementById("date-picker").value;
+      const datePickerEnd = document.getElementById("date-picker-end").value
 
-    return { title, textArea ,datePicker};
+    return { title, textArea, datePicker ,datePickerEnd};
 
 }
 
@@ -27,33 +28,114 @@ const randomColor = () => {
 // user can add task , remove , edit 
 
 
+// setValue
+
+const setValue = (value = "dayGridMonth") => {
+
+    return value;
+
+
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const taskDB = [];
     const calendarEl = document.getElementById('calendar');
 
+
+    // button
+
+    const buttonSection = document.getElementById("button-section");
+    console.log(buttonSection);
+
+
+
+
+
+
+
+
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
+        // initialView: 'dayGridMonth',
+        initialView: setValue(),
         editable: true,
         selectable: true,
-        events: [], // start empty
-        dateClick: function () {
+        events: [],
+
+        eventDidMount: function (info) {
+
+            // delete
+
+            const deleteBtn = document.createElement('button');
+            deleteBtn.innerText = 'X',
+                deleteBtn.style.height = "20px"
+            deleteBtn.style.marginTop = '10px'
+            deleteBtn.style.width = "40px"
+
+            deleteBtn.addEventListener("click", (e) => {
+                e.stopPropagation();
+                info.event.remove();
+
+            })
+            console.log(info)
+            info.el.style.height = '60px';
+            info.el.style.display = 'flex justifiy-content:between'
+            info.el.style.fontSize = "18px"
+            info.el.overflowX = "auto"
+
+            info.el.appendChild(deleteBtn);
+
+
+
+
+        },
+
+
+        dateClick: function (info) {
+            console.log(info)
             alert('A day has been clicked!');
         }
     });
 
     calendar.render();
 
+    // change calander
+    document.getElementById("year").addEventListener("click", () => {
+        calendar.changeView("dayGridYear"); 
+    });
+
+    document.getElementById("month").addEventListener("click", () => {
+        calendar.changeView("dayGridMonth");
+    });
+
+    document.getElementById("week").addEventListener("click", () => {
+        calendar.changeView("dayGridWeek");
+    });
+
+
+
+
     const btn = document.getElementById("add");
     btn.addEventListener("click", (e) => {
         e.preventDefault();
-        const { title, textArea,datePicker } = fromValue();
+        const { title, textArea, datePicker ,datePickerEnd} = fromValue();
+
+        if(!title  || !textArea  || !datePicker || !datePickerEnd)
+        {
+            alert("Please Enter your full info");
+            return;
+        }
 
         const task = {
+
             title,
             description: textArea,
             date: datePicker,
             id: randomID(),
-            color: randomColor()
+            color: randomColor(),
+            end:datePickerEnd,
         };
 
         taskDB.push(task);
@@ -61,10 +143,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Add the task to FullCalendar
         calendar.addEvent({
+            id: task.id,
             title: task.title,
             start: task.date,
-            color: task.color
+            color: task.color,
+            end: task.end,
         });
-        
+
     });
 });
